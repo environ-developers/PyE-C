cf5d32bd2443ea27e80a99fb5743a885db70a3f3
Edan Bainglass <edan.bainglass@gmail.com>
2021-11-01 10:11:40 -0500

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import os
-import subprocess
-import pathlib
-import shutil
+
+from pathlib import Path
+from shutil import rmtree, copy2
+from subprocess import check_call
 
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
@@ -16,14 +17,18 @@ long_description = """pyec turns Environ into an engine for embedding or for any
 
 
 class MakeBuild(build_ext):
+    """
+    Build instructions for wrapping Environ's Fortran code. Executes targets
+    from Makefile.
+    """
     def run(self):
+        """Executes build."""
         for ext in self.extensions:
             self.build_extension(ext)
 
     def build_extension(self, ext):
+        """Builds extension."""
         topdir = './'
-        build_args = []
-        env = os.environ.copy()
         self.build_name = self.build_lib + os.sep + name
 
         try:
@@ -31,66 +36,79 @@ class MakeBuild(build_ext):
             nprocs = mp.cpu_count()
         except ImportError:
             nprocs = 1
-        build_args += ['-j', str(nprocs)]
 
         # cleanup
-        if os.path.exists(self.build_temp): 
-            shutil.rmtree(self.build_temp)
-        if not os.path.exists(self.build_temp): 
-            os.makedirs(self.build_temp)
+        if os.path.exists(self.build_temp): rmtree(self.build_temp)
+        os.makedirs(self.build_temp)
 
         # remove *.so files
-        for f in pathlib.Path(self.build_temp).glob('*.so'):
-            os.remove(f)
+        for file in Path(self.build_temp).glob('*.so'):
+            os.remove(file)
 
-        makefiles = list(pathlib.Path(topdir + '/install/').glob('*')) + \
-                    list(pathlib.Path(topdir + '/src/').glob('**/*.f90'))
+        # copy makefiles to temporary build dir
+        makefiles = list(Path(topdir + '/install/').glob('*'))
+        makefiles += list(Path(topdir + '/src/').glob('**/*.f90'))
 
-        for f in makefiles:
-            shutil.copy2(f, self.build_temp)
+        for makefile in makefiles:
+            copy2(makefile, self.build_temp)
 
-        subprocess.check_call(['make', '-f', 'Makefile'] + build_args, cwd=self.build_temp, env=env)
+        # execute makefiles
+        check_call(
+            ['make', '-f', 'Makefile'] + ['-j', str(nprocs)],
+            cwd=self.build_temp,
+            env=os.environ.copy(),
+        )
 
-        if not os.path.exists(self.build_lib): os.makedirs(self.build_lib)
-        if not os.path.exists(self.build_name): os.makedirs(self.build_name)
-        for f in pathlib.Path(self.build_temp + os.sep + name + os.sep).glob('*'):
-            shutil.copy2(f, self.build_name)
-        for f in pathlib.Path(self.build_temp).glob('*.so'):
-            shutil.copy2(f, self.build_lib + os.sep)
+        # make build dirs
+        for path in (self.build_lib, self.build_name):
+            if not os.path.exists(path): os.makedirs(path)
+
+        # copy files to build dir
+        files = Path(self.build_temp + os.sep + name + os.sep).glob('*')
+
+        for file in files:
+            copy2(file, self.build_name)
+
+        # copy libraries to lib dir
+        for files in Path(self.build_temp).glob('*.so'):
+            copy2(files, self.build_lib + os.sep)
 
 
 extensions_pyec = Extension(
-        "pyec._pyec",
-        sources = [],
-        )
+    "pyec._pyec",
+    sources=[],
+)
 
-ext_modules = [extensions_pyec, ]
+ext_modules = [
+    extensions_pyec,
+]
 
 if __name__ == "__main__":
+
     setup(
-            name=name,
-            url='',
-            description=description,
-            version=__version__,
-            author=__author__,
-            author_email=__contact__,
-            license=__license__,
-            long_description=long_description,
-            python_requires = '>=3.6',
-            install_requires=['numpy>=1.18.0', 'f90wrap>=0.2.3'],
-            extras_require={
-                'mpi': [
-                    'mpi4py>=3.0.0',
-                    ],
-                },
-            packages=find_packages('./'),
-            ext_modules=ext_modules,
-            cmdclass = {"build_ext" : MakeBuild},
-            classifiers=[
-                'Development Status :: 1 - Beta',
-                'Intended Audience :: Science/Research',
-                'Programming Language :: Python :: 3',
-                'Topic :: Scientific/Engineering :: Chemistry',
-                'Topic :: Scientific/Engineering :: Physics'
-                ],
-            )
+        name=name,
+        url='',
+        description=description,
+        version=__version__,
+        author=__author__,
+        author_email=__contact__,
+        license=__license__,
+        long_description=long_description,
+        python_requires='>=3.6',
+        install_requires=['numpy>=1.18.0', 'f90wrap>=0.2.3'],
+        extras_require={
+            'mpi': [
+                'mpi4py>=3.0.0',
+            ],
+        },
+        packages=find_packages('./'),
+        ext_modules=ext_modules,
+        cmdclass={"build_ext": MakeBuild},
+        classifiers=[
+            'Development Status :: 1 - Beta',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 3',
+            'Topic :: Scientific/Engineering :: Chemistry',
+            'Topic :: Scientific/Engineering :: Physics'
+        ],
+    )
